3d97bec38f923152594ebcb3bc6e5e1f41b88041
studentgoit <ivansed16@gmail.com>
2016-10-22 18:42:22 +0300

    First commit

---
+++ src/src/TestFile.java
@@ -0,0 +1,10 @@
+/**
+ * Created by studentgoit on 22.10.16.
+ */
+public class TestFile {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Hello World :)");
+    }
+}
