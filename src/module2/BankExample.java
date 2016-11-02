944e9ac458ac176d491f125bb2675c7a3929fc25
Sidorii <ivansed16@gmail.com>
2016-11-02 20:15:53 +0200

    task 2.2

---
+++ src/module2/BankExample.java
@@ -0,0 +1,29 @@
+package module2;
+
+/**
+ * Created by Иван on 02.11.2016.
+ */
+public class BankExample {
+
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+    //HOMEWORK 2.1
+    static double withdrawBalance(double balance, double withdrawal) {
+
+
+        return 0;
+    }
+
+    //HOMEWORK 2.2
+    static double withdrawBalance(String ownerName, double withdrawal) {
+
+
+        return 0;
+    }
+
+    //HOMEWORK 2.3
+    static double fundBalance(String ownerName, double fund) {
+
+        return 0;
+    }
+}
--- src/module2/Task1.java
+++ src/module2/Task1.java
@@ -3,63 +3,34 @@ package module2;
 import java.util.Arrays;
 
 public class Task1 {
-    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
-    static int balancesSum(int[] balances) {
-        int sum = 0;
-        for (int balance : balances) {
-            if (balance > 1000 && balance < 5000) {
-                sum += balance;
-            }
-        }
-        return sum;
-    }
+    public static void main(String[] args) {
 
-    static String[] getOwnerNames(int[] balances, String[] names) {
-        int ownersCount = 0;
-        for (int balance : balances) {
-            if (balance > 1000) ownersCount++;
-        }
+        int[] balances = {1200, 250, 2000, 500, 3200};
 
-        String[] result = new String[ownersCount];
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
-        int index = 0;
-        for (int i = 0; i < balances.length; i++) {
-                if (balances[i] > 1000) {
-                    result[index] = names[i];
-                    index++;
-                }
-        }
-
-        return result;
-    }
-
-    ///*тип возвращаемого значения*/void  /*название метода*/ (/*параметры*/) {
-        //логика
-
-        //return //что-то
-    //}
-
-    int testMethod(int element, double test1, char pool) {
-        //logic
-        return 0;
-    }
-
-    void testMethod2(int test, String name) {
-        //logic
-        System.out.println("test....");
-
+        withdrawBalance(100,10);
+        withdrawBalance(100,99);
 
     }
 
-    //параметры
-    //тип значения название параметра
 
     //HOMEWORK 2.1
     static double withdrawBalance(double balance, double withdrawal) {
 
+        double commision = 0.05;
 
-        return 0;
+
+        double resultBalance = balance - withdrawal - (withdrawal*commision);
+
+        if (resultBalance < 0){
+            System.out.println("NO");
+        }else {
+            System.out.println("OK " + commision + " " + resultBalance);
+        }
+
+        return resultBalance;
     }
 
     //HOMEWORK 2.2
@@ -74,18 +45,4 @@ public class Task1 {
 
         return 0;
     }
-
-    public static void main(String[] args) {
-
-        int[] balances = {1200, 250, 2000, 500, 3200};
-
-        System.out.println(Arrays.toString(balances));
-
-
-        int[] balances1 = {200, 200, 500, 4000, 30};
-
-    }
-
-
-
 }
