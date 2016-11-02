b013c0d41807ceee4148e86e881776717f041d36
Sidorii <ivansed16@gmail.com>
2016-11-02 20:46:41 +0200

    task2.3

--- src/module2/BankExample.java
+++ src/module2/BankExample.java
@@ -5,25 +5,98 @@ package module2;
  */
 public class BankExample {
 
-    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+    private static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+    private static int[] balances = {1200, 250, 2000, 500, 3200};
+
+    public static void main(String[] args) {
+
+        withdrawBalance(100,10);
+        withdrawBalance(100,99);
+
+        System.out.println();
+
+        withdrawBalance("Ann",100);
+        withdrawBalance("Oww",490);
+        withdrawBalance("Ivan",1000);
+
+        System.out.println();
+
+        fundBalance("Oww",100);
+        fundBalance("Ann",-20);
+        fundBalance("Ivan",100);
+    }
+
 
     //HOMEWORK 2.1
     static double withdrawBalance(double balance, double withdrawal) {
 
+        double commision = 0.05;
 
-        return 0;
+
+        double resultBalance = balance - withdrawal - (withdrawal*commision);
+
+        if (resultBalance < 0){
+            System.out.println("NO");
+            return balance;
+        }else {
+            System.out.println("OK " + commision + " " + resultBalance);
+            return resultBalance;
+        }
+
+
     }
 
     //HOMEWORK 2.2
     static double withdrawBalance(String ownerName, double withdrawal) {
 
+        int pointer = findUserByName(ownerName);
 
-        return 0;
+
+        if (pointer == ownerNames.length){
+            System.out.println("User not defined!");
+            return 0;
+        }
+
+        double commision  = .05d;
+
+        double result = balances[pointer] - withdrawal - (withdrawal*commision);
+
+        if (result < 0){
+            System.out.println(ownerNames[pointer] + " NO");
+            return balances[pointer];
+        }else {
+            System.out.println(ownerNames[pointer] + " " + withdrawal + " " + result);
+            return result;
+        }
+    }
+
+    static int findUserByName(String name){
+        int pointer;
+
+        for(pointer = 0; pointer < ownerNames.length;pointer++)
+            if (name.equals(ownerNames[pointer]))
+                break;
+        return pointer;
     }
 
     //HOMEWORK 2.3
     static double fundBalance(String ownerName, double fund) {
 
-        return 0;
+        if (fund < 0) {
+            System.out.println("Access denied!");
+            return 0;
+        }
+
+        int pointer = findUserByName(ownerName);
+
+        if (pointer == ownerNames.length){
+            System.out.println("User not defined!");
+            return 0;
+        }
+
+        balances[pointer] += fund;
+        System.out.println(ownerName + " " + balances[pointer]);
+
+        return balances[pointer];
     }
 }
