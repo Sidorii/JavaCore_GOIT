573de6b1cefc3e69297ea3c6959d3ad02eb90a3c
Sidorii <ivansed16@gmail.com>
2016-11-02 20:18:58 +0200

    Update BankExample.java

--- src/module2/BankExample.java
+++ src/module2/BankExample.java
@@ -5,25 +5,31 @@ package module2;
  */
 public class BankExample {
 
-    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+    public static void main(String[] args) {
+
+        int[] balances = {1200, 250, 2000, 500, 3200};
+
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        withdrawBalance(100,10);
+        withdrawBalance(100,99);
+
+    }
+
 
     //HOMEWORK 2.1
     static double withdrawBalance(double balance, double withdrawal) {
 
+        double commision = 0.05;
 
-        return 0;
+
+        double resultBalance = balance - withdrawal - (withdrawal*commision);
+
+        if (resultBalance < 0){
+            System.out.println("NO");
+        }else {
+            System.out.println("OK " + commision + " " + resultBalance);
+        }
+
+        return resultBalance;
     }
-
-    //HOMEWORK 2.2
-    static double withdrawBalance(String ownerName, double withdrawal) {
-
-
-        return 0;
-    }
-
-    //HOMEWORK 2.3
-    static double fundBalance(String ownerName, double fund) {
-
-        return 0;
-    }
-}
